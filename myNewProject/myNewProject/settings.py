e033020a146e00c73ef53c0de3c427657ea40405
GaneshJavas <rjaiganesh01@gmail.com>
2024-10-13 22:40:51 +0530

    13.10.2024

--- myNewProject/myNewProject/settings.py
+++ myNewProject/myNewProject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pokemon',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
