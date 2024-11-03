1741bec9521df35e79233a268329bcb832d2ea8c
GaneshJavas <rjaiganesh01@gmail.com>
2024-11-03 15:44:07 +0530

    3 sept

--- myNewProject/pokemon/admin.py
+++ myNewProject/pokemon/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import PokemonViewer
+from .models import PokemonViewer, PokemonRating, PokemonRegion, PokemonDiscovery
 # Register your models here.
 
-admin.site.register(PokemonViewer)
+# admin.site.register(PokemonViewer)
+
+class pokemon_rating(admin.TabularInline):
+    model = PokemonRating
+    extra = 2
+
+class all_pokemons(admin.ModelAdmin):
+    display_pokemon = ('id','name', 'poke_type')
+    inlines = [pokemon_rating]
+
+class pokemon_region(admin.ModelAdmin):
+    display_region = ('Name',)
+    filter_horizontal = ('poke_in_my_region',)
+
+class pokemon_discovery(admin.ModelAdmin):
+    display_cert = ('Certificate',)
+
+admin.site.register(PokemonViewer,all_pokemons)
+admin.site.register(PokemonRegion,pokemon_region)
+admin.site.register(PokemonDiscovery,pokemon_discovery)
--- myNewProject/pokemon/models.py
+++ myNewProject/pokemon/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class PokemonViewer(models.Model):
@@ -22,3 +23,27 @@ class PokemonViewer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PokemonRating(models.Model):
+    poke = models.ForeignKey(PokemonViewer, on_delete=models.CASCADE, related_name="rating")
+    poke_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.poke_user.username} Rated for {self.poke.name}'
+
+class PokemonRegion(models.Model):
+    poke_in_my_region = models.ManyToManyField(PokemonViewer, related_name="region")
+    region_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.region_name
+    
+class PokemonDiscovery(models.Model):
+    poke_discovered = models.OneToOneField(PokemonViewer, on_delete=models.CASCADE, related_name="disc")
+    discovery_date = models.DateTimeField(default=timezone.now())
+    disc_cert = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'Certificate for {self.poke_discovered.name}'
